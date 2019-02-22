c60f8d79a54eb0143363a49ce39631eaefa4b4ca
Nightmayr <umayr23@gmail.com>
2019-02-22 11:39:19 +0000

    added script to clone repos

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+git clone https://github.com/Nightmayr/FootballManager-Frontend.git
+git clone https://github.com/Nightmayr/FootballManager-AccountApi.git
+git clone https://github.com/Nightmayr/FootballManager-PlayerManager.git
+git clone https://github.com/Nightmayr/FootballManager-Email.git
+git clone https://github.com/Nightmayr/FootballManager-Consumer.git
+git clone https://github.com/Nightmayr/FootballManager-MongoClientService.git
